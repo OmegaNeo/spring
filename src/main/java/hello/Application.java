27678b0f69982c7b456ca8b5565c0ef92e978e93
OmegaNeo <55429482+OmegaNeo@users.noreply.github.com>
2019-12-02 15:57:31 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -27,13 +27,13 @@ public class Application extends SpringBootServletInitializer {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-              System.out.println("abc");
+              logger.log("abc");
             
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+                logger.log(beanName);
             }
 
         };
